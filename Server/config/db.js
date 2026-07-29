778fcc060352279229872e4a2a84d3a38b5ceeff
Harshini <harshinivsvs@gmail.com>
2026-07-29 18:34:46 +0530

    Update database configuration

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,22 +1,30 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
+console.log("Host:", process.env.DB_HOST);
+console.log("Port:", process.env.DB_PORT);
+console.log("User:", process.env.DB_USER);
+console.log("Database:", process.env.DB_NAME);
+
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectTimeout: 30000,
 });
 
 db.connect((err) => {
-    if (err) {
-        console.log("❌ Database Connection Failed");
-        console.log(err);
-        return;
-    }
-
+  if (err) {
+    console.error("❌ Database Connection Failed");
+    console.error(err);
+  } else {
     console.log("✅ MySQL Connected Successfully");
+  }
 });
 
 module.exports = db;
