975c7559141e7ba166d2a55cddf86692cd4f9351
Harshini <harshinivsvs@gmail.com>
2026-07-29 12:54:18 +0530

    Deplyment

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "harshini",
-    database: "college_event_management"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
